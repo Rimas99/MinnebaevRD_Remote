3a54b0861b2f37f46d7a1a9cbd06e4b4a3ee616f
Rimas99 <rimtat99@gmail.com>
2025-06-01 16:46:29 +0300

    delete solve task2

--- MinnebaevRD/Program.cs
+++ MinnebaevRD/Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 // Миннебаев Рим Дамирович Ам-1-24
-//solve task2
 static public double Sqr(double X)
 {
     return X * X;
