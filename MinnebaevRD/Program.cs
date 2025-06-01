fcc5d733ebf7ca789559360a8007ea5c3bc7d2ff
Rimas99 <rimtat99@gmail.com>
2025-06-01 16:46:07 +0300

    add new feature

--- MinnebaevRD/Program.cs
+++ MinnebaevRD/Program.cs
@@ -2,3 +2,8 @@
 Console.WriteLine("Hello, World!");
 // Миннебаев Рим Дамирович Ам-1-24
 //solve task2
+static public double Sqr(double X)
+{
+    return X * X;
+}
+Console.WriteLine("Квадрат числа 18 = " + Sqr(18));
